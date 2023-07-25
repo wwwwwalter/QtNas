4309c13106c7506c6cc4f17c87ed57be1c9f4b23
Albert <15221024927@163.com>
2023-07-26 03:34:15 +0800

    add logo
    Signed-off-by: Albert <15221024927@163.com>

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,8 +1,12 @@
 #include "mainwindow.h"
 
+#include <QIcon>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    //main widget
+    setWindowIcon(QIcon(":/images/logo.png"));
 }
 
 MainWindow::~MainWindow()
